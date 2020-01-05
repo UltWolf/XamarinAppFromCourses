c2d01c30264d88584509c7352360496dddb08847
Zayarniy Vitaliy <ultwolf@gmail.com>
2020-01-05 15:59:23 +0200

    Chapter 1 have been ended

--- XamarinAppFromCourses/XamarinAppFromCourses/MainPage.xaml.cs
+++ XamarinAppFromCourses/XamarinAppFromCourses/MainPage.xaml.cs
@@ -10,8 +10,7 @@ using XamarinAppFromCourses.ViewModels;
 
 namespace XamarinAppFromCourses
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
+ 
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
