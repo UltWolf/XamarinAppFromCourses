82929e1d7ee0d1e9f850eedac44ab7a9581fa0ab
Zayarniy Vitaliy <ultwolf@gmail.com>
2020-01-05 17:42:03 +0200

    chapter 2 finish

--- Chapter2/MainActivity.cs
+++ Chapter2/MainActivity.cs
@@ -12,49 +12,38 @@ namespace Chapter2
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-
+        EditText inputBill;
+        Button calculateButton;
+        TextView outputTip;
+        TextView outputTotal;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
-
             Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
+            inputBill = FindViewById<EditText>(Resource.Id.inputBill);
 
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+
         }
-
-        public override bool OnCreateOptionsMenu(IMenu menu)
+        void OnCalculateClick(object sender, EventArgs e)
         {
-            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-            return true;
-        }
-
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            int id = item.ItemId;
-            if (id == Resource.Id.action_settings)
+            string text = inputBill.Text;
+            double bill = 0;
+            if (double.TryParse(text, out bill))
             {
-                return true;
+                var tip = bill * 0.15;
+                var total = bill + tip;
+                outputTip.Text = tip.ToString();
+                outputTotal.Text = total.ToString();
             }
 
-            return base.OnOptionsItemSelected(item);
-        }
-
-        private void FabOnClick(object sender, EventArgs eventArgs)
-        {
-            View view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
+        } 
 	}
 }
 
